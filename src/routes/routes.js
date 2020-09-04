19b125917fdaf2108c6a06b2f7ef0562255e1121
anbreaker <antunez19@gmail.com>
2020-09-04 12:36:27 +0200

    subiendo imagenes a cloudinary

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,12 @@
 const {Router} = require('express');
-const path = require('path');
 const router = Router();
+const fs = require('fs-extra');
+
+// Model of DB
+const Photo = require('../models/Photo');
+
+// Cloudinary
+const cloudinary = require('./configCloudinary');
 
 router.get('/', (req, res) => {
   // res.send('Hola');
@@ -11,6 +17,26 @@ router.get('/images/add', (req, res) => {
   // res.sendFile(path.join(__dirname, '../views/layouts/main.html'));
   // res.render('../views/partials/image_form');
   res.render('partials/image_form');
+});
+
+router.post('/images/add', async (req, res) => {
+  // console.log(req.body);
+  const {title, description} = req.body;
+  // console.log(req.file);
+
+  const result = await cloudinary.v2.uploader.upload(req.file.path);
+  // console.log(result);
+
+  const newPhoto = new Photo({
+    title,
+    description,
+    imageURL: result.secure_url,
+    public_id: result.public_id,
+  });
+  await newPhoto.save();
+
+  res.send('World');
+  await fs.unlink(req.file.path);
 });
 
 module.exports = router;
