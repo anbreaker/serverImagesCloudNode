d4212c583f8c97726038b51a30531de56169c41d
anbreaker <antunez19@gmail.com>
2020-09-04 14:07:26 +0200

    subiendo imagenes y guradado en mongo ok

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,15 +8,17 @@ const Photo = require('../models/Photo');
 // Cloudinary
 const cloudinary = require('./configCloudinary');
 
-router.get('/', (req, res) => {
-  // res.send('Hola');
-  res.render('partials/images');
+router.get('/', async (req, res) => {
+  const photos = await Photo.find().lean();
+  // console.log(photos);
+  res.render('partials/images', {photos});
 });
 
-router.get('/images/add', (req, res) => {
+router.get('/images/add', async (req, res) => {
   // res.sendFile(path.join(__dirname, '../views/layouts/main.html'));
   // res.render('../views/partials/image_form');
-  res.render('partials/image_form');
+  const photos = await Photo.find().lean();
+  res.render('partials/image_form', {photos});
 });
 
 router.post('/images/add', async (req, res) => {
@@ -34,9 +36,20 @@ router.post('/images/add', async (req, res) => {
     public_id: result.public_id,
   });
   await newPhoto.save();
-
-  res.send('World');
+  // Delete image of uploads local
   await fs.unlink(req.file.path);
+
+  res.redirect('/');
+});
+
+router.get('/images/delete/:photo_id', async (req, res) => {
+  const {photo_id} = req.params;
+  const photoDeleted = await Photo.findByIdAndDelete(photo_id);
+
+  //Delete photo on cloudinary
+  const result = await cloudinary.v2.uploader.destroy(photoDeleted.public_id);
+  console.log(result);
+  res.redirect('/images/add');
 });
 
 module.exports = router;
